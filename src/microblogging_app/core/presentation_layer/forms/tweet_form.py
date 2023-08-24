5cd267d25f1660e2186662d3eab51a71b38787aa
KaterinaPy35 <yakushka1308@mail.ru>
2023-08-24 10:27:44 +0300

    Added home and trending in your country references in nav bar. Changed available number of tags for tweets.

--- src/microblogging_app/core/presentation_layer/forms/tweet_form.py
+++ src/microblogging_app/core/presentation_layer/forms/tweet_form.py
@@ -6,4 +6,4 @@ class TweetForm(forms.Form):
     """An implementation of a tweet (reply of a tweet) form."""
 
     content = forms.CharField(label="Content", widget=forms.Textarea, max_length=400)
-    tags = forms.CharField(label="Tags", widget=forms.Textarea, validators=[TagsNumberValidator(max_number=5)])
+    tags = forms.CharField(label="Tags", widget=forms.Textarea, validators=[TagsNumberValidator(max_number=20)])
